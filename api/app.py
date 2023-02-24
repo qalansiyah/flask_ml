3317b00feccf8d6816f4a47a46f40883034e069f
Arthur <arthur@pop-os.localdomain>
2023-02-24 14:44:59 +0500

    8

---
+++ api/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+import pickle
+import sklearn
+from sklearn.linear_model import LinearRegression
+from numpy import abs
+
+
+
+app = Flask(__name__, template_folder='templates')
+@app.route('/', methods = ['POST', 'GET'])
+def main():
+    y_pred = ''
+    if request.method == 'POST':
+        exp = float(request.form['experience'])
+        y_pred = loaded_model.predict([[exp]])
+        y_pred = abs(y_pred)
+    return render_template('main.html', result = y_pred)
+
+
+if __name__ == '__main__':
+    with open('lr_model.pkl', 'rb') as f:
+        loaded_model = pickle.load(f)
